02f65deef3dc0c134d58cd72964e6b4ccdac401d
sahilnikalje <sahilnik88@gmail.com>
2026-03-19 14:53:17 +0530

    Implement MyBookings page with booking data display and loading state; add date formatting utility

--- Frontend/src/assets/assets.js
+++ Frontend/src/assets/assets.js
@@ -292,47 +292,47 @@ export const dummyDashboardData = {
 }
 
 
-// export const dummyBookingData = [
-//     {
-//         "_id": "68396334fb83252d82e17295",
-//         "user": { "name": "GreatStack", },
-//         "show": {
-//             _id: "68352363e96d99513e4221a4",
-//             movie: dummyShowsData[0],
-//             showDateTime: "2025-06-30T02:30:00.000Z",
-//             showPrice: 59,
-//         },
-//         "amount": 98,
-//         "bookedSeats": ["D1", "D2"],
-//         "isPaid": false,
-//     },
-//     {
-//         "_id": "68396334fb83252d82e17295",
-//         "user": { "name": "GreatStack", },
-//         "show": {
-//             _id: "68352363e96d99513e4221a4",
-//             movie: dummyShowsData[0],
-//             showDateTime: "2025-06-30T02:30:00.000Z",
-//             showPrice: 59,
-//         },
-//         "amount": 49,
-//         "bookedSeats": ["A1"],
-//         "isPaid": true,
-//     },
-//     {
-//         "_id": "68396334fb83252d82e17295",
-//         "user": { "name": "GreatStack", },
-//         "show": {
-//             _id: "68352363e96d99513e4221a4",
-//             movie: dummyShowsData[0],
-//             showDateTime: "2025-06-30T02:30:00.000Z",
-//             showPrice: 59,
-//         },
-//         "amount": 147,
-//         "bookedSeats": ["A1", "A2","A3"],
-//         "isPaid": true,
-//     },
-// ]
+export const dummyBookingData = [
+    {
+        "_id": "68396334fb83252d82e17295",
+        "user": { "name": "GreatStack", },
+        "show": {
+            _id: "68352363e96d99513e4221a4",
+            movie: dummyShowsData[0],
+            showDateTime: "2025-06-30T02:30:00.000Z",
+            showPrice: 59,
+        },
+        "amount": 98,
+        "bookedSeats": ["D1", "D2"],
+        "isPaid": false,
+    },
+    {
+        "_id": "68396334fb83252d82e17295",
+        "user": { "name": "GreatStack", },
+        "show": {
+            _id: "68352363e96d99513e4221a4",
+            movie: dummyShowsData[0],
+            showDateTime: "2025-06-30T02:30:00.000Z",
+            showPrice: 59,
+        },
+        "amount": 49,
+        "bookedSeats": ["A1"],
+        "isPaid": true,
+    },
+    {
+        "_id": "68396334fb83252d82e17295",
+        "user": { "name": "GreatStack", },
+        "show": {
+            _id: "68352363e96d99513e4221a4",
+            movie: dummyShowsData[0],
+            showDateTime: "2025-06-30T02:30:00.000Z",
+            showPrice: 59,
+        },
+        "amount": 147,
+        "bookedSeats": ["A1", "A2","A3"],
+        "isPaid": true,
+    },
+]
 
 // import logo from './logo.svg'
 // import marvelLogo from './marvelLogo.svg'
