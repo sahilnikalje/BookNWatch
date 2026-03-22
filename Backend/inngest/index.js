608da0963b7eb0f18dde239b789ded990a6044e0
sahilnikalje <sahilnik88@gmail.com>
2026-03-22 23:35:27 +0530

    backend added

---
+++ Backend/inngest/index.js
@@ -0,0 +1,59 @@
+const {Inngest}=require('inngest')
+const User=require('../models/userModel')
+
+ const inngest = new Inngest({ id: "movie-ticket-booking" })
+
+//todo inngest function to save user data to db
+const syncUserCreation=inngest.createFunction(
+    {id:'sync-user-from-clerk'},
+    {event: 'clerk/user.created'},
+
+    async({event})=>{
+        const {id, first_name, last_name, email_addresses, image_url}=event.data
+        const userData={
+            _id:id,
+            email:email_addresses[0].email_address,
+            name:first_name + " " + last_name,
+            image:image_url
+        }
+        await User.create(userData)
+    }
+)
+
+//todo inngest function to delete user from db
+const syncUserDeletion=inngest.createFunction(
+    {id:'delete-user-with-clerk'},
+    {event: 'clerk/user.deleted'},
+
+    async({event})=>{
+        const {id}=event.data
+        await User.findByIdAndDelete(id)
+    }
+)
+
+//todo inngest function to update the user data in db
+const syncUserUpdation=inngest.createFunction(
+    {id:'update-user-from-clerk'},
+    {event: 'clerk/user.updated'},
+
+    async({event})=>{
+        const {id, first_name, last_name, email_addresses, image_url}=event.data
+
+        const userData={
+            _id:id,
+            email:email_addresses[0].email_address,
+            name:first_name + " " + last_name,
+            image:image_url
+        }
+        await User.findByIdAndUpdate(id, userData)
+    }
+)
+
+
+
+
+
+//todo pass function here
+ const functions = [syncUserCreation, syncUserDeletion, syncUserUpdation]
+
+ module.exports={inngest, functions}
